c462b5d05bacab9167bbffa0650ac3de34bfacb2
Mary <omota4fidelity@gmail.com>
2023-09-28 20:29:31 +0100

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0:
+        print ([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    
+       
     pass
